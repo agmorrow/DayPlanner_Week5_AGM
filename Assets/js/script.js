6a54d05d287e8a1e04bad74ba9a3f3a439e60a10
agmorrow <morrow.austin5@gmail.com>
2022-02-16 02:02:03 -0700

    Timeblock works

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,29 @@
+const description = $('.description');
+const saveBtn = $('.saveBtn');
 let currentDay = $('#currentDay');
-let currentTime = moment().format('dddd, MMMM Do');
+let currentDate = moment().format('dddd, MMMM Do');
+let currentTime = moment().format('H');
+
+
+  
+
+console.log(currentTime);
+
+currentDay.text(currentDate);
+
+
+function compareTimes() {
+  for (let i = 9; i < 18; i++) {
+    console.log(i);
+  if (currentTime > i) {
+    $('#timeBlock' + (i)).find('.description').addClass('past');
+  } else if (currentTime < i) {
+    $('#timeBlock' + (i)).find('.description').addClass('future'); 
+  } else {
+    $('#timeBlock' + (i)).find('.description').addClass('present');
+}
+}};
 
 
 
-currentDay.text(currentTime);
+compareTimes();
