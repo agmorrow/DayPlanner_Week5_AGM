6f2814092cb778e4d51a2b280332e76f015959a6
agmorrow <morrow.austin5@gmail.com>
2022-02-22 22:33:27 -0700

    localstorage works

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,42 @@
-const description = $('.description');
-const saveBtn = $('.saveBtn');
 let currentDay = $('#currentDay');
 let currentDate = moment().format('dddd, MMMM Do');
 let currentTime = moment().format('H');
 
+$(currentDay).html(currentDate);
 
-  
+$(document).ready(function () {
+  // saveBtn click listener 
+  $(".saveBtn").on("click", function () {
+    // Get nearby values of the description in JQuery
+    let eventText = $(this).siblings(".description").val();
+    let timeBlock = $(this).parent().attr("id");
+    // Save text in local storage
+    localStorage.setItem(timeBlock, eventText);
 
-console.log(currentTime);
-
-currentDay.text(currentDate);
-
+  })
+});
 
 function compareTimes() {
   for (let i = 9; i < 18; i++) {
     console.log(i);
-  if (currentTime > i) {
-    $('#timeBlock' + (i)).find('.description').addClass('past');
-  } else if (currentTime < i) {
-    $('#timeBlock' + (i)).find('.description').addClass('future'); 
-  } else {
-    $('#timeBlock' + (i)).find('.description').addClass('present');
-}
-}};
+    if (currentTime > i) {
+      $('#timeBlock' + (i)).find('.description').addClass('past');
+    } else if (currentTime < i) {
+      $('#timeBlock' + (i)).find('.description').addClass('future');
+    } else {
+      $('#timeBlock' + (i)).find('.description').addClass('present');
+    }
 
-
+    $('#timeBlock9 .description').val(localStorage.getItem('timeBlock9'));
+    $('#timeBlock10 .description').val(localStorage.getItem('timeBlock10'));
+    $('#timeBlock11 .description').val(localStorage.getItem('timeBlock11'));
+    $('#timeBlock12 .description').val(localStorage.getItem('timeBlock12'));
+    $('#timeBlock13 .description').val(localStorage.getItem('timeBlock13'));
+    $('#timeBlock14 .description').val(localStorage.getItem('timeBlock14'));
+    $('#timeBlock15 .description').val(localStorage.getItem('timeBlock15'));
+    $('#timeBlock16 .description').val(localStorage.getItem('timeBlock16'));
+    $('#timeBlock17 .description').val(localStorage.getItem('timeBlock17'));
+  }
+};
 
 compareTimes();
